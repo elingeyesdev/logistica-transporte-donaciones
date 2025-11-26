54844b3dcf0151b4dc65dcc767eb844975a0c944
Isabella Parada <isvbellv26@gmail.com>
2025-11-26 00:16:47 -0400

    boton volver en seguimiento

--- resources/views/seguimiento/tracking.blade.php
+++ resources/views/seguimiento/tracking.blade.php
@@ -5,7 +5,12 @@
             <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
             @endsection
 @section('content_header')
-    <h1>Tracking del Paquete: {{ $paquete->codigo }}</h1>
+    <div class="d-flex justify-content-between align-items-center">
+        <h1>Tracking del Paquete: {{ $paquete->codigo }}</h1>
+        <a href="{{ route('paquete.index') }}" class="btn btn-primary">
+            <i class="fas fa-arrow-left"></i> Volver
+        </a>
+    </div>
 @endsection
 
 
